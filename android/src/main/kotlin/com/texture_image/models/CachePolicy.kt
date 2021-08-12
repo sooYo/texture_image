1549f31d1c6f96e4a86897a4e8f08435179e4366
sooyo <xiao.chu.go@163.com>
2021-08-12 15:06:09 +0800

    Reformat CachePolicy

--- android/src/main/kotlin/com/texture_image/models/CachePolicy.kt
+++ android/src/main/kotlin/com/texture_image/models/CachePolicy.kt
@@ -1,27 +1,15 @@
 package com.texture_image.models
 
 import coil.request.CachePolicy
-import coil.request.ImageRequest
 
 class CachePolicy(
-    private val memoryCache: Boolean = true,
-    private val diskCache: Boolean = true,
-    private val networkCache: Boolean = true,
+    memoryCache: Boolean = false,
+    diskCache: Boolean = true,
+    networkCache: Boolean = true,
 ) {
-    val coilDiskCache: CachePolicy
-        get() {
-            return toCoilCachePolicy(diskCache)
-        }
-
-    val coilMemCache: CachePolicy
-        get() {
-            return toCoilCachePolicy(memoryCache)
-        }
-
-    val coilNetworkCache: CachePolicy
-        get() {
-            return toCoilCachePolicy(networkCache)
-        }
+    val coilDiskCache: CachePolicy = toCoilCachePolicy(diskCache)
+    val coilMemCache: CachePolicy = toCoilCachePolicy(memoryCache)
+    val coilNetworkCache: CachePolicy = toCoilCachePolicy(networkCache)
 
     private fun toCoilCachePolicy(enable: Boolean): CachePolicy {
         return when {
