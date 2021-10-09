165582541ef45949b7fad72e6c9335131ae29a94
sooyo <xiao.chu.go@163.com>
2021-10-09 18:31:04 +0800

    Move iOS project generated files

---
+++ example/ios/GeneratedPluginRegistrant.m
@@ -0,0 +1,33 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<path_provider/FLTPathProviderPlugin.h>)
+#import <path_provider/FLTPathProviderPlugin.h>
+#else
+@import path_provider;
+#endif
+
+#if __has_include(<sqflite/SqflitePlugin.h>)
+#import <sqflite/SqflitePlugin.h>
+#else
+@import sqflite;
+#endif
+
+#if __has_include(<texture_image/TextureImagePlugin.h>)
+#import <texture_image/TextureImagePlugin.h>
+#else
+@import texture_image;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FLTPathProviderPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTPathProviderPlugin"]];
+  [SqflitePlugin registerWithRegistrar:[registry registrarForPlugin:@"SqflitePlugin"]];
+  [TextureImagePlugin registerWithRegistrar:[registry registrarForPlugin:@"TextureImagePlugin"]];
+}
+
+@end
