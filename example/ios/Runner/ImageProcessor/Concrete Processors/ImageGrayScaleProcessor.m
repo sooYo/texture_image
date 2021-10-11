e36878a1a7c927c07cf6c907c2b6a939d2477ed8
sooyo <xiao.chu.go@163.com>
2021-10-11 17:58:26 +0800

    Add grayscale processor

--- example/ios/Runner/ImageProcessor/Concrete Processors/ImageGrayScaleProcessor.m
+++ example/ios/Runner/ImageProcessor/Concrete Processors/ImageGrayScaleProcessor.m
@@ -1,12 +1,53 @@
 //
 //  ImageGrayScaleProcessor.m
-//  Runner
+//  TextureImage
 //
 //  Created by Sooyo on 2021/10/11.
 //
 
 #import "ImageGrayScaleProcessor.h"
+#import "ImageRequest.h"
+
+#import "Macros.h"
 
 @implementation ImageGrayScaleProcessor
+
+@dynamic identifier;
+
+- (NSString *)identifier {
+    return NSStringFromClass(self.class);
+}
+
+- (void)processImage:(UIImage *)image request:(id<ImageRequestProtocol>)request withCompletion:(ImageProcessCompletion)completion {
+    @autoreleasepool {
+        size_t width = image.size.width * image.scale;
+        size_t height = image.size.height * image.scale;
+        
+        // Use gray color space to redraw the target image
+        CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceGray();
+        CGContextRef context = CGBitmapContextCreate(nil, width, height, 8, 0, colorSpace, kCGBitmapByteOrderDefault);
+        
+        CGColorSpaceRelease(colorSpace);
+
+        if (context == NULL) {
+            TIExecBlock(completion, nil, [TIError processorErrorForType:self.identifier url:request.url]);
+            return;
+        }
+        
+        // Redraw target image
+        CGContextDrawImage(context, CGRectMake(0, 0, width, height), image.CGImage);
+        
+        // Turn into UIImage
+        CGImageRef cgImage = CGBitmapContextCreateImage(context);
+        UIImage *result = [UIImage imageWithCGImage:cgImage scale:image.scale orientation:image.imageOrientation];
+    
+        CGImageRelease(cgImage);
+        CGContextRelease(context);
+        
+        TIExecBlock(completion, result, nil);
+    }
+}
+     
+
 
 @end
